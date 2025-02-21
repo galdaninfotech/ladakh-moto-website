6dcb3f769e56705a33a3adaff4e367a89842535c
Galdan Infotech <galdaninfotech@gmail.com>
2025-02-21 08:31:57 +0530

    Edited Secret key

--- src/env.d.ts
+++ src/env.d.ts
@@ -2,7 +2,7 @@
 
 interface ImportMetaEnv {
   readonly VITE_RECAPTCHA_SITE_KEY: string
-  readonly VITE_RECAPTCHA_SECRET_KEY: string
+  readonly RECAPTCHA_SECRET_KEY: string
 }
 
 interface ImportMeta {
