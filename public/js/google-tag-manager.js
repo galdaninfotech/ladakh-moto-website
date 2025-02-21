2dedb96bcb7bec9c69845ca48b3dfd0411541bc3
Galdan Infotech <galdaninfotech@gmail.com>
2025-02-21 12:27:00 +0530

    Fixed Additioonal domain in GTM

--- public/js/google-tag-manager.js
+++ public/js/google-tag-manager.js
@@ -2,4 +2,15 @@
     new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
     j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
     'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-    })(window,document,'script','dataLayer','GTM-NFMMQ5LD');
+})(window,document,'script','dataLayer','GTM-NFMMQ5LD');
+
+// Add domain configuration
+window.dataLayer = window.dataLayer || [];
+dataLayer.push({
+    'gtm.allowlist': ['ladakhmoto.com', 'www.ladakhmoto.com'],
+    'gtm.blocklist': undefined,
+    'customDomainConfig': {
+        'allowedDomains': ['ladakhmoto.com', 'www.ladakhmoto.com'],
+        'cookieDomain': 'ladakhmoto.com'
+    }
+});
